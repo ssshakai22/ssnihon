8fb2947afdfdf502f8758ad9676fb5f423441d1d
skazuki120 <sk8350250@outlook.jp>
2021-07-04 22:29:57 +0900

    Initial Commit

---
+++ ssnihon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ssnihon
+//
+//  Created by 小路一樹 on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
